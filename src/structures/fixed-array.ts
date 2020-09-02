f23c5c7b7864c086a6cf702e4ad23063fc36987c
emsknock <25252659+emsknock@users.noreply.github.com>
2020-09-02 19:27:29 +0300

    Autoformat

--- src/structures/fixed-array.ts
+++ src/structures/fixed-array.ts
@@ -23,21 +23,21 @@ export class FixedArray<T> {
     public size = () => this._size;
 
     /** Get an element in the array by its index */
-    public get (i: number) {
-        if(!this.isInBounds(i))
+    public get(i: number) {
+        if (!this.isInBounds(i))
             throw ReferenceError(`Index out of bounds for array size ${this._size}: ${i}`);
         return this._array[i];
     }
 
     /** Set a value in the array by index */
     public set(i: number, v: T) {
-        if(!this.isInBounds(i))
+        if (!this.isInBounds(i))
             throw ReferenceError(`Index out of bounds for array size ${this._size}: ${i}`);
         this._array[i] = v;
     }
 
     public *[Symbol.iterator]() {
-        for(let i = 0; i < this._size; i++) {
+        for (let i = 0; i < this._size; i++) {
             yield this._array[i];
         }
     }
