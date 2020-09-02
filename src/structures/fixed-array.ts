d8bac788ad229fe069686131a9416ed96076e174
emsknock <25252659+emsknock@users.noreply.github.com>
2020-09-02 19:22:44 +0300

    Add documentation to FixedArray

--- src/structures/fixed-array.ts
+++ src/structures/fixed-array.ts
@@ -1,3 +1,12 @@
+/**
+ * A basic array with a fixed size.
+ * 
+ * JS arrays are dynamic by default, so a custom implementation is required to fulfill
+ * the course requirements.
+ * 
+ * This implementation uses a JS array under the hood, which is explicitly allowed.
+ * 
+ */
 export class FixedArray<T> {
 
     private _array: T[] = [];
@@ -7,16 +16,20 @@ export class FixedArray<T> {
         this._size = size;
     }
 
+    /** Guard to block attempts to index the array out of bounds */
     private isInBounds = (i: number) => !(i < 0) && i < this._size;
 
+    /** Get the size of the array. The max index is this value minus one. */
     public size = () => this._size;
 
+    /** Get an element in the array by its index */
     public get (i: number) {
         if(!this.isInBounds(i))
             throw ReferenceError(`Index out of bounds for array size ${this._size}: ${i}`);
         return this._array[i];
     }
 
+    /** Set a value in the array by index */
     public set(i: number, v: T) {
         if(!this.isInBounds(i))
             throw ReferenceError(`Index out of bounds for array size ${this._size}: ${i}`);
