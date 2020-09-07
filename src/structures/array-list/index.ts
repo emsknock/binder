ac27930d28814975a79770b8136698e21e840c23
emsknock <25252659+emsknock@users.noreply.github.com>
2020-09-07 17:07:05 +0300

    Add documentation to ArrayList

--- src/structures/array-list/index.ts
+++ src/structures/array-list/index.ts
@@ -11,9 +11,11 @@ export class ArrayList<T> {
 
 	private isInBounds = (i: number) => !(i < 0) && i < this._size;
 
+	/** Returns true if this list has an element with the given index */
 	public has = (i: number) => this.isInBounds(i);
 	public size = () => this._size;
 
+	/** Returns the value and the index of the head element */
 	public getHead = () => {
 		if (this._size < 1) throw ReferenceError("Cannot get head of an empty array");
 		return {
@@ -21,6 +23,7 @@ export class ArrayList<T> {
 			index: 0
 		};
 	}
+	/** Returns the value and the index of the tail element */
 	public getTail = () => {
 		if (this._size < 1) throw ReferenceError("Cannot get tail of an empty array");
 		return {
@@ -29,15 +32,18 @@ export class ArrayList<T> {
 		};
 	}
 
+	/** Get an element by index */
 	public get(i: number) {
 		if (!this.isInBounds(i)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${i}`);
 		return this._array.get(i);
 	}
+	/** Set an element by index */
 	public set(i: number, v: T) {
 		if (!this.isInBounds(i)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${i}`);
 		this._array.set(i, v);
 	}
 
+	/** Add a value to the end of the list */
 	public add(v: T) {
 
 		const newIdx = this._size;
@@ -51,6 +57,7 @@ export class ArrayList<T> {
 
 	}
 
+	/** Remove an element from the list by index */
 	public remove(i: number) {
 
 		if (!this.isInBounds(i)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${i}`);
@@ -62,6 +69,7 @@ export class ArrayList<T> {
 
 	}
 
+	/** Removes and returns the last element in the list */
 	public popTail = () => {
 
 		if (this._size < 1) throw ReferenceError("Cannot pop from an empty array");
@@ -73,6 +81,7 @@ export class ArrayList<T> {
 
 	}
 
+	/** Swap two elements in the list by their indices */
 	public swapByIndex(aIdx: number, bIdx: number) {
 		if (!this.isInBounds(aIdx)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${aIdx}`);
 		if (!this.isInBounds(bIdx)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${bIdx}`);
