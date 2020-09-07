16f2b60c686c3bb9ef5641088c22f3462e1bb13c
emsknock <25252659+emsknock@users.noreply.github.com>
2020-09-07 17:04:26 +0300

    Refactoring ArrayList

--- src/structures/array-list/index.ts
+++ src/structures/array-list/index.ts
@@ -10,49 +10,41 @@ export class ArrayList<T> {
 	}
 
 	private isInBounds = (i: number) => !(i < 0) && i < this._size;
-	private tailIdx = () => this._size - 1;
 
+	public has = (i: number) => this.isInBounds(i);
 	public size = () => this._size;
 
 	public getHead = () => {
-		if (this._size < 1)
-			throw ReferenceError("Cannot get head of an empty array");
-		return { value: this._array.get(0), index: 0 };
+		if (this._size < 1) throw ReferenceError("Cannot get head of an empty array");
+		return {
+			value: this._array.get(0),
+			index: 0
+		};
 	}
 	public getTail = () => {
-		if (this._size < 1)
-			throw ReferenceError("Cannot get tail of an empty array");
-		return { value: this._array.get(this.tailIdx()), index: this.tailIdx() };
-	};
-
-	public popTail = () => {
-		if (this._size < 1)
-			throw ReferenceError("Cannot pop from an empty array");
-		const tail = this.getTail().value;
-		this._size--;
-		return tail;
+		if (this._size < 1) throw ReferenceError("Cannot get tail of an empty array");
+		return {
+			value: this._array.get(this._size - 1),
+			index: this._size - 1
+		};
 	}
-
-	public has = (i: number) => this.isInBounds(i);
 
 	public get(i: number) {
-		if (!this.isInBounds(i))
-			throw ReferenceError(`Index out of bounds for List size ${this._size}: ${i}`);
+		if (!this.isInBounds(i)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${i}`);
 		return this._array.get(i);
 	}
-
 	public set(i: number, v: T) {
-		if (!this.isInBounds(i))
-			throw ReferenceError(`Index out of bounds for List size ${this._size}: ${i}`);
+		if (!this.isInBounds(i)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${i}`);
 		this._array.set(i, v);
 	}
 
 	public add(v: T) {
 
 		const newIdx = this._size;
-		const arrLen = this._array.size();
-		if (newIdx === arrLen)
-			this._array = this._array.copy(arrLen * 2);
+		const size = this._array.size();
+
+		if (newIdx === size)
+			this._array = this._array.copy(size * 2);
 
 		this._array.set(newIdx, v);
 		this._size++;
@@ -61,8 +53,7 @@ export class ArrayList<T> {
 
 	public remove(i: number) {
 
-		if (!this.isInBounds(i))
-			throw ReferenceError(`Index out of bounds for List size ${this._size}: ${i}`);
+		if (!this.isInBounds(i)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${i}`);
 
 		for (let j = i + 1; j < this._array.size(); j++)
 			this._array.set(j - 1, this._array.get(j));
@@ -71,11 +62,20 @@ export class ArrayList<T> {
 
 	}
 
+	public popTail = () => {
+
+		if (this._size < 1) throw ReferenceError("Cannot pop from an empty array");
+
+		const value = this.get(this._size - 1);
+		this.remove(this._size - 1);
+
+		return value;
+
+	}
+
 	public swapByIndex(aIdx: number, bIdx: number) {
-		if (!this.isInBounds(aIdx))
-			throw ReferenceError(`Index out of bounds for List size ${this._size}: ${aIdx}`);
-		if (!this.isInBounds(bIdx))
-			throw ReferenceError(`Index out of bounds for List size ${this._size}: ${bIdx}`);
+		if (!this.isInBounds(aIdx)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${aIdx}`);
+		if (!this.isInBounds(bIdx)) throw ReferenceError(`Index out of bounds for List size ${this._size}: ${bIdx}`);
 		const a = this._array.get(aIdx);
 		const b = this._array.get(bIdx);
 		this._array.set(aIdx, b);
