febf00878dc669c13cec64e2ee204e0899d00406
emsknock <25252659+emsknock@users.noreply.github.com>
2020-10-04 21:01:50 +0300

    Remove has method and return undefined if not found instead

--- src/structures/bucket/index.ts
+++ src/structures/bucket/index.ts
@@ -11,27 +11,8 @@ export class Bucket<K extends Identifiable, V> {
 
     private list = new ArrayList<{ key: K, value: V }>();
 
-    public add(key: K, value: V) {
-        this.list.add({ key, value });
-    }
+    public add = (key: K, value: V) => this.list.add({ key, value });
 
-    private fetch(key: K) {
-        return this.list.find(x => x.key.id === key.id);
-    }
-
-    public has(key: K) {
-        return typeof this.fetch(key) !== "undefined";
-    }
-
-    public find(key: K) {
-
-        const element = this.fetch(key);
-
-        if (!element)
-            throw Error(`No such key in bucket: ${key.id}`);
-
-        return element;
-
-    }
+    public find = (key: K)  => this.list.find(x => x.key.id === key.id);
 
 }
