bf87c9b4b83270b602029d1b223eb5c71dd2493d
emsknock <25252659+emsknock@users.noreply.github.com>
2020-10-04 21:04:01 +0300

    Create Dictionary structure

---
+++ src/structures/dictionary/index.ts
@@ -0,0 +1,27 @@
+import { FixedArray } from "structures/fixed-array";
+import { Bucket } from "structures/bucket";
+
+import { Identifiable } from "types/identifiable";
+
+export class Dictionary<K extends Identifiable, V> {
+
+    private _buckets = new FixedArray<Bucket<K, V>>(256, () => new Bucket());
+
+    private getBucket = (key: K) => this._buckets.get(key.id % 256);
+
+    public set(key: K, value: V) {
+        this.getBucket(key).add(key, value);
+    }
+
+    public get(key: K) {
+        const value = this.getBucket(key).find(key);
+        if (!value)
+            throw Error(`No such key in dictionary: ${key.id}`);
+        return value;
+    }
+
+    public getSafe(key: K, defaultValue: V) {
+        return this.getBucket(key).find(key) ?? defaultValue;
+    }
+
+}
