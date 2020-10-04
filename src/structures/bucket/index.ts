3f0d2f5d6b8ee702a33302528f6abcc070611d72
emsknock <25252659+emsknock@users.noreply.github.com>
2020-10-04 20:50:07 +0300

    Use ArrayList instead of internal linked list

--- src/structures/bucket/index.ts
+++ src/structures/bucket/index.ts
@@ -1,3 +1,4 @@
+import { ArrayList } from "structures/array-list";
 import { Identifiable } from "types/identifiable";
 
 interface BucketNode<K extends Identifiable, V> {
@@ -8,37 +9,21 @@ interface BucketNode<K extends Identifiable, V> {
 
 export class Bucket<K extends Identifiable, V> {
 
-    private root: BucketNode<K, V> | null = null;
-    private last: BucketNode<K, V> | null = null;
+    private list = new ArrayList<{ key: K, value: V }>();
 
     public add(key: K, value: V) {
-        const node: BucketNode<K, V> = { key, value };
-        if (!this.root) {
-            this.root = node;
-            this.last = node;
-        } else {
-            this.last!.next = node;
-        }
+        this.list.add({ key, value });
     }
 
     public find(key: K) {
 
-        const idToFind = key.id();
-        
-        if (!this.root)
-            throw Error(`Trying to find key from empty bucket: ${idToFind}`);
+        const idToFind = key.id;
+        const element = this.list.find(x => x.key.id === idToFind);
 
-        let node = this.root;
-        while (node.key.id() !== idToFind) {
-            
-            if(!node.next)
-                throw Error(`No such key in bucket: ${idToFind}`);
+        if (!element)
+            throw Error(`No such key in bucket: ${idToFind}`);
 
-            node = node.next;
-            
-        }
-
-        return node;
+        return element;
 
     }
 
