e131728ad1c135cd4cf958efdee273831b7d375e
emsknock <emswidn@gmail.com>
2020-10-20 17:42:25 +0300

    Update tests to match new method name

--- src/utils/buffer-reader/buffer-reader.test.ts
+++ src/utils/buffer-reader/buffer-reader.test.ts
@@ -11,7 +11,7 @@ it("calculates bytes left correctly when removing a byte at a time", () => {
 
     for (let i = content.length; i > 0; i--) {
         expect(a.bytesLeft()).toEqual(i);
-        a.readUntil(() => true);
+        a.readBytesUntil(() => true);
     }
 
 });
@@ -23,13 +23,13 @@ it("calculates bytes left correctly when removing different sized slices", () =>
 
     expect(a.bytesLeft()).toEqual(content.length);
 
-    a.readUntil(s => s.size() === 2);
+    a.readBytesUntil(s => s.size() === 2);
     expect(a.bytesLeft()).toEqual(content.length - 2);
 
-    a.readUntil(s => s.size() === 40);
+    a.readBytesUntil(s => s.size() === 40);
     expect(a.bytesLeft()).toEqual(content.length - 2 - 40);
 
-    a.readUntil(s => s.size() === 200);
+    a.readBytesUntil(s => s.size() === 200);
     expect(a.bytesLeft()).toEqual(content.length - 2 - 40 - 200);
 
 });
@@ -38,10 +38,10 @@ it("returns slices correctly when in bounds of the buffer", () => {
 
     const a = $(0xDE, 0xAD, 0xBE, 0xEF, 0xC0, 0xFF, 0xEE, 0x00);
 
-    const r1 = toJsArray(a.readUntil(s => s.size() === 2));
-    const r2 = toJsArray(a.readUntil(s => s.size() === 2));
-    const r3 = toJsArray(a.readUntil(s => s.size() === 3));
-    const r4 = toJsArray(a.readUntil(s => s.size() === 1));
+    const r1 = toJsArray(a.readBytesUntil(s => s.size() === 2));
+    const r2 = toJsArray(a.readBytesUntil(s => s.size() === 2));
+    const r3 = toJsArray(a.readBytesUntil(s => s.size() === 3));
+    const r4 = toJsArray(a.readBytesUntil(s => s.size() === 1));
     expect(r1).toEqual([0xDE, 0xAD]);
     expect(r2).toEqual([0xBE, 0xEF]);
     expect(r3).toEqual([0xC0, 0xFF, 0xEE]);
@@ -51,28 +51,28 @@ it("returns slices correctly when in bounds of the buffer", () => {
 
 it("passes correct slices to the predicate", () => {
     const a = $(0xDE, 0xAD, 0xBE, 0xEF, 0xC0, 0xFF, 0xEE, 0x00);
-    a.readUntil(s => {
+    a.readBytesUntil(s => {
         if (s.size() === 2) {
             expect(toJsArray(s)).toEqual([0xDE, 0xAD]);
             return true;
         }
         return false;
     });
-    a.readUntil(s => {
+    a.readBytesUntil(s => {
         if (s.size() === 2) {
             expect(toJsArray(s)).toEqual([0xBE, 0xEF]);
             return true;
         }
         return false;
     });
-    a.readUntil(s => {
+    a.readBytesUntil(s => {
         if (s.size() === 3) {
             expect(toJsArray(s)).toEqual([0xC0, 0xFF, 0xEE]);
             return true;
         }
         return false;
     });
-    a.readUntil(s => {
+    a.readBytesUntil(s => {
         if (s.size() === 1) {
             expect(toJsArray(s)).toEqual([0x00]);
             return true;
@@ -84,5 +84,5 @@ it("passes correct slices to the predicate", () => {
 it("returns the rest of the buffer when going out of bounds", () => {
     const arr = [0xDE, 0xAD, 0xBE, 0xEF, 0xC0, 0xFF, 0xEE, 0x00];
     const a = $(...arr);
-    expect(toJsArray(a.readUntil(() => false))).toEqual(arr);
+    expect(toJsArray(a.readBytesUntil(() => false))).toEqual(arr);
 });
